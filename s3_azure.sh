ab2ff9aca1667b1984d0e8428636df6170e92d44
Sunil Aryal <sunilaryal33@gmail.com>
2024-11-27 14:01:27 +0100

    Create s3_azure.sh

---
+++ s3_azure.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+while getopts i:w:n:k:c:r option
+do
+    case "${option}"
+    in
+        i) fileslist=${OPTARG};;
+        n) azure_storage_name=${OPTARG};;
+        k) azure_storage_key=${OPTARG};;
+        c) azure_container_name=${OPTARG};;
+        r) aws_container_name=${OPTARG};;
+    esac
+done
+
+echo "Getting names of files"
+files=`cat $1`
+i=0
+total_files=`wc -l $1`
+
+start_date_time="`date +%Y%m%d%H%M%S`";
+
+for file in $files; do
+    echo "Uploading the file..."
+    az storage blob copy start --source-uri https://s3.$aws_container_name.amazonaws.com/$azure_container_name/$file --destination-blob $file --destination-container weather --account-key $azure_storage_key --account-name $azure_storage_name  > /dev/null
+    i=`expr $i + 1`
+    echo "Total Files Uploaded $i / $total_files"
+    current_date_time="`date +%Y%m%d%H%M%S`";
+    echo $current_date_time;
+done
+
+echo "Files in the Azure container"
+az storage blob list --container-name $azure_container_name --output table
+
+current_date_time="`date +%Y%m%d%H%M%S`";
+echo "start date: $start_date_time";
+echo "end date: $current_date_time";
